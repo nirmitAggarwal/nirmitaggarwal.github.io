0c49f023aee55a34a86d3751e48c17bbdd5cf445
nirmit152007 <kiitcommon@gmail.com>
2024-08-04 08:56:40 +0530

    03

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,10 +6,39 @@ import {logo, menu, close} from "../assets";
 
 
 const Navbar = () => {
+  const [active, setActive] = useState("");
+
+
   return (
-    <nav className={`${styles.paddingX} w-full flex items-center py-5 top-0 z-20 bg-primary`}>
+    <nav className={`${styles.paddingX} w-full px-0 flex items-center fixed py-5 top-0 z-20 bg-primary`}>
       <div className="w-full flex justify-between items-center max-w-7x1 mx-auto">
-        
+          <Link to={"/"} className='flex items-center gap-2' onClick={()=>{
+            setActive('');
+            window.scrollTo(0,0);
+
+          }}>
+            <img src={logo} alt='logo' className='w-9 h-9 object-contain'/>
+            <p className='text-white text-[18px] font-bold cursor-pointer'>Nirmit<span className='sm:block hidden'>| Sparks</span></p>
+          </Link>
+          <ul className="list-none hidden sm:flex flex-row gap-10">
+            {navLinks.map((link) => (
+              <li 
+              key={link.id} 
+              className={`${active === link.title ? "text-white" : "text-secondary"} hover:text-white text-[18px] font-medium cursor-pointer`} 
+              onClick={() => setActive(link.title)}
+              
+              >
+                <a href={`#${link.id}`}>
+                {link.title}
+                </a>
+              </li>
+              
+            ))}
+          </ul>
+
+          <div className="sm:hidden flex flex-1 justify-end items-center">
+            <img src={menu} alt="menu" className='w-[28px] h-[28px]'/>
+          </div>
       </div>
     </nav>
   )
